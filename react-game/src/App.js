43cf8e264229c9455c5303ed6ef1809711355154
mstars_lab2_11 <lab_02_01@Lab-02-01s-iMac.local>
2022-04-14 10:54:29 +0800

    aa

---
+++ react-game/src/App.js
@@ -0,0 +1,35 @@
+
+// import { useEffect, useState } from 'react';
+// import './App.css';
+
+// function App() {
+
+//   const [data , setData] = useState([])
+
+//   useEffect(()=>{
+
+//     fetch('http://localhost:300/data')
+//     .then(res => res.json())
+//     .then(data => {
+//       setData(data)
+//       console.log(data);
+//     })
+//   }, [])
+//   return (
+//     <div className="App">
+//       {/* <div className="board"></div> */}
+//       {data.map(e=>{
+//         return (
+//           <>
+//         <p>{e.name}</p>
+//         <p>{e.age}</p>
+//           </>
+//         )
+//       })}
+      
+
+//     </div>
+//   );
+// }
+
+// export default App;
