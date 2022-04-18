5964d88142f345b8f38447f8841d594a14a62893
mstars_lab2_11 <lab_02_01@Lab-02-01s-iMac.local>
2022-04-18 15:01:48 +0800

    aa

--- react-game/src/App1.js
+++ react-game/src/App1.js
@@ -7,40 +7,70 @@ export default function App1() {
   const [array1, setArray1] = useState([
     ["1", "2", "3"],
     ["4", "5", "6"],
-    ["7", "8", "0"],
+    ["7", "0", "8"],
   ]);
-  const [id1, setId1] = useState(0)
-
-  function handleChange(id){
-    setId1(id)
+  const [changed, setChanged] = useState(false);
+  function shuffle(array) { 
+    return array.sort(() => Math.random() - 0.5);
   }
+  
+  const [pos, setPos] = useState()
+
   useEffect(()=>{
-    console.log(id1);
+    setArray1(array1.map(row=>shuffle(row)))
+    setPos(findZeroClicked())
+    // debugger
+  },[])
+
+  
+
+  function handleChange(id,rowIndex,colIndex) {
+    console.log(rowIndex,colIndex)
+    if (validation(id,rowIndex,colIndex)) {
+      // findPosClicked(id);
+      // console.log(clicked)
+      let temp = array1;
+      let temp1 = temp[pos[0]][pos[1]];
+      temp[pos[0]][pos[1]] = temp[rowIndex][colIndex];
+      temp[rowIndex][colIndex] = temp1;
+
+      setPos([rowIndex,colIndex])
+      setArray1(temp);
+      setChanged(!changed);
+    }
+  }
+
+
+  function findZeroClicked() {
     let row = array1.length;
     let col = array1[0].length;
     for (let i = 0; i < row; i++) {
-        for(let j = 0; j < col; j++){
-            if(array1[i][j] == id1){
-                console.log(i, j);
-                
-                let arrays =  array1
-                arrays[i][j] = '0'
-                console.log(arrays);
-                setArray1(arrays)
-                // console.log(array1);
-            }
+      for (let j = 0; j < col; j++) {
+        if (array1[i][j] == "0") {
+          return [i, j];
         }
-        
+      }
     }
-  }, [id1])
+  }
 
+  function validation(id,row,col) {
+    console.log("row = ", row)
+    console.log("col = ", col)
+    console.log('sdave');
+
+
+
+
+    return true;
+  }
 
   return (
     <div>
+
       <div className="board">
-        {array1.map((e) => (
-          <Item value={e} key={e} func={handleChange}/>
-        ))}
+        {array1.map((row,rowIndex) =>
+          row.map((col, i) => <Item key={i} data={col} rowIndex = {rowIndex} colIndex={i} func={handleChange} />)
+        )}
       </div>
     </div>
   );
